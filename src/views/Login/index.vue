88eac776f9adbe776e761bd66a38552850908259
paixinxin <1765356051@qq.com>
2020-06-02 12:15:19 +0800

    加了vant

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,13 +1,44 @@
 <template>
-  <div>Login登录页面复苏阶段和考试考试分数</div>
+  <div>
+    <van-form @submit="onSubmit">
+      <van-field
+        v-model="username"
+        name="用户名"
+        label="用户名"
+        placeholder="用户名"
+        :rules="[{ required: true, message: '请填写用户名' }]"
+      />
+      <van-field
+        v-model="password"
+        type="password"
+        name="密码"
+        label="密码"
+        placeholder="密码"
+        :rules="[{ required: true, message: '请填写密码' }]"
+      />
+      <div style="margin: 16px;">
+        <van-button round block type="info" native-type="submit">提交</van-button>
+      </div>
+    </van-form>
+  </div>
 </template>
-
 <script>
+import Vue from "vue";
+import { Form,Field,Button } from "vant";
+
+Vue.use(Button).use(Field).use(Form);
+
 export default {
-
-}
+   data() {
+    return {
+      username: '',
+      password: '',
+    };
+  },
+  methods: {
+    onSubmit(values) {
+      console.log('submit', values);
+    },
+  },
+};
 </script>
-
-<style>
-
-</style>
